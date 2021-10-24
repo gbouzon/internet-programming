c1cd0ec9087ef7b54c4d8b0a420d2eea8e3bb402
Giuliana Bouzon <ga@bouzon.com.br>
2021-10-24 03:54:37 -0400

    started on getName() - fairly finished

--- Assignments/Assignment 05 - Form Validation/script.js
+++ Assignments/Assignment 05 - Form Validation/script.js
@@ -1,4 +1,26 @@
+//clean getCheckboxes and getName() when ya get some time pls
+//generalize this stuff laters
+
+function getCheckboxes() {
+    var pets = document.querySelectorAll('input[name="pets"]:checked');
+    var petStr = "";
+
+    for (var i = 0; i < pets.length; i++)
+        petStr += pets[i].value + " ";
+    
+    return petStr;
+}
+
 function getName() {
+    console.log("First Name is: " + document.getElementById("fName").value);
+    console.log("Last Name is: " + document.getElementById("lName").value);
+    console.log("Username is: " + document.getElementsByName("username")[0].value);
+    console.log("Password is: " + document.getElementsByName("pswd")[0].value);
+    console.log("Email is: " + document.getElementsByName("email")[0].value);
+    console.log("Gender is: " + document.querySelector('input[name="gender"]:checked').value);
+    console.log("Pets selected: " + getCheckboxes());
+    console.log("Comments is: " + document.getElementsByName("comments")[0].value);
+    console.log("Age is: " + document.querySelector('option:checked').value);
     //displays the value of ALL entered info in the console
 }
 

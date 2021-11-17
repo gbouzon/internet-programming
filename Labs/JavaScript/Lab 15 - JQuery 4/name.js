58a79bbdeeb7f4ccc6519bb9255f36b1633dc745
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-17 01:17:22 -0500

    added lab 15

---
+++ Labs/JavaScript/Lab 15 - JQuery 4/name.js
@@ -0,0 +1,28 @@
+//replicating ajax 1 lab but using jquery
+$(document).ready(function() {
+    $("button").click(function() {
+        //find another way to do this later
+        var data = "";
+        var inputs = document.querySelectorAll("input");
+        for (var i = 0; i < inputs.length; i++){
+            data += inputs[i].getAttribute("name"); 
+            data += "=";
+            data += inputs[i].value;
+            data += "&";
+        }
+        data = data.substring(0, data.length - 1);
+        console.log(data);
+        
+        var url = "name.txt";
+
+        //using POST method
+        //$.post(url, data, function(result) {
+        //    $("#response").html(result);
+        //});
+
+        //using GET method
+        $.get(url + "?" + data, function(result) {
+            $("#response").html(result);
+        });
+    });
+});
